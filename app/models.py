a975fb294c48c4bde18f1a9152a793483ebd6bc4
davidkiama <davidkiama98@gmail.com>
2022-03-07 11:12:49 +0300

    Added the upvotes and downvotes attributes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from cgitb import text
+
 from flask_login import UserMixin
 from app import db
 
@@ -11,11 +11,14 @@ class User (UserMixin, db.Model):
     profile_pic = db.Column(db.String(), nullable=False, default='default.jpg')
     pitches = db.relationship('Pitch', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    upvotes = db.relationship('Upvote', backref='user', passive_deletes=True)
+    downvotes = db.relationship(
+        'Downvote', backref='user', passive_deletes=True)
 
 
 class Pitch (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(255))
+    text = db.Column(db.String(3000))
     category = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     author = db.Column(db.Integer, db.ForeignKey(
@@ -23,12 +26,34 @@ class Pitch (db.Model):
     comments = db.relationship(
         'Comment', backref='pitch', passive_deletes=True)
 
+    upvotes = db.relationship('Upvote', backref='pitch', passive_deletes=True)
+    downvotes = db.relationship(
+        'Downvote', backref='pitch', passive_deletes=True)
+
 
 class Comment (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(300), nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     author = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete='CASCADE'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey(
+        'pitch.id', ondelete='CASCADE'), nullable=False)
+
+
+class Upvote (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer,)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete='CASCADE'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey(
+        'pitch.id', ondelete='CASCADE'), nullable=False)
+
+
+class Downvote (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer,)
+    user_id = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete='CASCADE'), nullable=False)
     pitch_id = db.Column(db.Integer, db.ForeignKey(
         'pitch.id', ondelete='CASCADE'), nullable=False)

57eca11836b2e12e1548672974e5cf1cd204b575
davidkiama <davidkiama98@gmail.com>
2022-03-07 12:15:59 +0300

    Test Deployment 6

--- app/email.py
+++ app/email.py
@@ -11,6 +11,8 @@ email_pwd = os.environ.get('EMAIL_PWD')
 def send_email(receiver_email, username):
     message = f'Welcome {username} to the Pitches App. Thanks for signing up!'
     server = smtplib.SMTP('smtp.gmail.com', 465)
+    server.ehlo()
     server.starttls()
     server.login(sender_email, email_pwd)
     server.sendmail(sender_email, receiver_email, message)
+    server.close()
